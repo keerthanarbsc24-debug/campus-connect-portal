6f0013c6e3a71094fdd7d2ff7105be19c951606a
Keerthana R <keerthanarbsc24@rvu.edu.in>
2026-09-07 10:43:44 +0530

    commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+
+// Mounts React specifically to your auth-root div in index.html
+createRoot(document.getElementById('auth-root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
